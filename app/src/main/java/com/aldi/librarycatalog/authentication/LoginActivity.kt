979115d81d1da12b084e4c28181e43baf1cc9eec
ganiaaldi <mrgad98@gmail.com>
2020-04-10 08:44:52 +0700

    implement form login ui

--- app/src/main/java/com/aldi/librarycatalog/authentication/LoginActivity.kt
+++ app/src/main/java/com/aldi/librarycatalog/authentication/LoginActivity.kt
@@ -1,13 +1,26 @@
 package com.aldi.librarycatalog.authentication
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.aldi.librarycatalog.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        buttonLogin.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
+        }
+        buttonBantuan.setOnClickListener {
+
+        }
     }
+
+
+
 }
